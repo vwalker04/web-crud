252e2c0b13c2c47025ad83c2272d9ecfad96d158
Vaughn Walker <vwalk04@gmail.com>
2024-11-06 10:38:26 -0500

    Add functionality to add new note

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid'
+
+const NOTES_KEY = 'notes'
 
 type Note = {
   id: string
@@ -15,20 +18,10 @@ export default function Home() {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
 
   useEffect(() => {
-    const newNote: Note = {
-      id: '2',
-      title: 'title',
-      content: 'foo',
-      createdTime: new Date().toISOString()
-    }
-    localStorage.setItem('notes', JSON.stringify(newNote))
-  }, [])
-
-  useEffect(() => {
-    const notesFromLocalStorage = localStorage.getItem('notes')
+    const notesFromLocalStorage = localStorage.getItem(NOTES_KEY)
     if (notesFromLocalStorage) {
       const parsedNotes = JSON.parse(notesFromLocalStorage)
-      setNotes([parsedNotes])
+      setNotes(parsedNotes)
     }
   }, [setNotes])
 
@@ -47,12 +40,36 @@ export default function Home() {
         })}
       </div>
       <Button onClick={() => setModalOpen(true)}>Add Note</Button>
-      <NotesModal open={modalOpen} handleClose={() => setModalOpen(false)} />
+      <NotesModal open={modalOpen} handleClose={() => setModalOpen(false)} setNotes={setNotes}/>
     </>
   );
 }
 
-const NotesModal = ({ open, handleClose }: { open: boolean, handleClose: () => void }) => {
+const NotesModal = ({ open, handleClose, setNotes }: { open: boolean, handleClose: () => void, setNotes: React.Dispatch<React.SetStateAction<Note[]>> }) => {
+  const [title, setTitle] = useState<string>("")
+  const [content, setContent] = useState<string>("")
+
+  const handleNewNote = () => {
+    const rawNotes = localStorage.getItem(NOTES_KEY)
+    try {
+      const foundNotes: Note[] = rawNotes ? JSON.parse(rawNotes) : []
+      const newNote: Note = {
+        id: uuidv4(),
+        title: title,
+        content: content,
+        createdTime: new Date().toISOString()
+      }
+      foundNotes.push(newNote)
+      localStorage.setItem(NOTES_KEY, JSON.stringify(foundNotes))
+      setNotes(foundNotes)
+    } catch (e: unknown) {
+      console.error(e)
+    } finally {
+      handleClose();
+    }
+
+  }
+
   return (
     <Modal
       open={open}
@@ -60,9 +77,9 @@ const NotesModal = ({ open, handleClose }: { open: boolean, handleClose: () => v
     >
       <Box>
         <Typography variant="h6">Add New Note</Typography>
-        <TextField label="Title" />
-        <TextField label="Content" />
-        <Button>Add Note</Button>
+        <TextField label="Title" value={title} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} />
+        <TextField label="Content" value={content} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setContent(e.target.value)} />
+        <Button onClick={handleNewNote}>Add Note</Button>
       </Box>
     </Modal>
   )
